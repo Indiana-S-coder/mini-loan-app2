619622fbbf2e7e2a30ddc5c58784f6f2f1c711f1
Bharti Patel <patelshree40@gmail.com>
2024-01-15 12:48:49 +0530

    production code addded

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,19 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error(err));
 
+// for production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 // middlwares
 app.use("/api/v1/auth", userRoute);
 app.use("/api/v1/loans", loanRoute);
