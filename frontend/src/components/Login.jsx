d32152a5b0919593100b433b0db810f2ed49acef
Bharti Patel <patelshree40@gmail.com>
2024-01-15 10:22:55 +0530

    login signup page styling done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../App";
-import { useNavigate } from "react-router-dom";
+import { Link ,useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
@@ -30,46 +30,48 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-slate-300 p-8 rounded-lg shadow-md w-full md:w-96">
-        <h2 className="text-2xl font-bold mb-4 text-slate-500">Login</h2>
+      <div className="border-t-red-500 border-t-8 bg-slate-50 p-8 rounded-lg shadow-md w-full md:w-96">
+        <h2 className="text-2xl font-bold mb-4 text-slate-600">Login</h2>
         <form className="space-y-4">
-          <div>
+          <div className="flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-600">
               Email:
             </label>
             <input
+              placeholder="Enter your email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-red-500"
             />
           </div>
-          <div>
+          <div className="flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-600">
               Password:
             </label>
             <input
+            placeholder="Enter your password"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-red-500"
             />
           </div>
           <button
             type="button"
             onClick={handleLogin}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200"
           >
             Login
           </button>
         </form>
         <div className="mt-4">
-          <a
-            href="/signup"
-            className="text-blue-500 hover:text-blue-700 focus:outline-none focus:underline"
-          >
-            Create new account?
-          </a>
+          <span> Create new account? <Link
+            to="/signup"
+            className="text-red-500 hover:text-red-700 focus:outline-none focus:underline">
+            Signup
+          </Link>
+          </span>
         </div>
       </div>
     </div>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../App";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -38,60 +39,63 @@ const Signup = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-slate-200 p-8 rounded-lg shadow-md w-full md:w-96">
+      <div className="border-t-red-500 border-t-8 bg-slate-50 p-8 rounded-lg shadow-md w-full md:w-96">
         <h2 className="text-2xl font-bold mb-4">Signup</h2>
         <form className="space-y-4">
-          <div>
+          <div className="flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-600">
               Name:
             </label>
             <input
+              placeholder="Enter your name"
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-red-500"
               required
             />
           </div>
-          <div>
+          <div className="flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-600">
               Email:
             </label>
             <input
+              placeholder="Enter your email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-red-500"
               required
             />
           </div>
-          <div>
+          <div className="flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-600">
               Password:
             </label>
             <input
+              placeholder="Enter your password"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              className="mt-1 p-2 w-full border border-gray-300 rounded focus:outline-none focus:border-red-500"
               required
             />
           </div>
           <button
             type="button"
             onClick={(e) => handleSignup(e)}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200"
           >
             Signup
           </button>
         </form>
         <div className="mt-4">
-          <a
-            href="/login"
-            className="text-blue-500 hover:text-blue-700 focus:outline-none focus:underline"
-          >
-            Already have an account?
-          </a>
+          <span> Already have account? <Link
+            to="/login"
+            className="text-red-500 hover:text-red-700 focus:outline-none focus:underline">
+            Login
+          </Link>
+          </span>
         </div>
       </div>
     </div>
