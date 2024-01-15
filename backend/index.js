d0822918b508c064a7612b80c846c6895e70befa
Bharti Patel <patelshree40@gmail.com>
2024-01-15 14:01:14 +0530

    vercel.jsom correction made

--- backend/index.js
+++ backend/index.js
@@ -20,18 +20,12 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error(err));
 
-// for production
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
+  app.get('*',(req,res,next)=>{
+    res.status(200).json({
+      message:'bad request'
+    })
   })
-}
+  
 
 // middlwares
 app.use("/api/v1/auth", userRoute);
